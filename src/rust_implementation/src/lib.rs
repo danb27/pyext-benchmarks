8f22905e2d44a78a5a0331fab95dc509f708105d
Steven Kessler <stvnksslr@gmail.com>
2024-11-07 16:24:39 -0500

    chore(formatting):

--- src/rust_implementation/src/lib.rs
+++ src/rust_implementation/src/lib.rs
@@ -1,30 +1,29 @@
-use std::collections::HashMap;
 use hashbrown::HashMap as HBHashMap;
 use pyo3::prelude::*;
+use std::collections::HashMap;
 
 #[pyfunction]
 fn bigrams(text: &str) -> PyResult<Vec<String>> {
     // Pre-split and collect to avoid multiple allocations
     let words: Vec<_> = text.split_whitespace().collect();
     let len = words.len();
-    
+
     if len < 2 {
         return Ok(Vec::new());
     }
-    
+
     // Single allocation with exact capacity
     let mut bigrams = Vec::with_capacity(len - 1);
-    
+
     // Use windows for more efficient iteration
-    words.windows(2)
-        .for_each(|pair| {
-            // Avoid separate format! allocation by using with_capacity
-            let mut s = String::with_capacity(pair[0].len() + pair[1].len() + 1);
-            s.push_str(pair[0]);
-            s.push(' ');
-            s.push_str(pair[1]);
-            bigrams.push(s);
-        });
+    words.windows(2).for_each(|pair| {
+        // Avoid separate format! allocation by using with_capacity
+        let mut s = String::with_capacity(pair[0].len() + pair[1].len() + 1);
+        s.push_str(pair[0]);
+        s.push(' ');
+        s.push_str(pair[1]);
+        bigrams.push(s);
+    });
 
     Ok(bigrams)
 }
@@ -35,13 +34,13 @@ fn two_sum_n_squared(nums: Vec<i32>, target: i32) -> PyResult<Vec<i32>> {
     if len < 2 {
         return Ok(Vec::new());
     }
-    
+
     // Use unchecked access for better performance in release mode
     unsafe {
-        for i in 0..len-1 {
+        for i in 0..len - 1 {
             let num_i = *nums.get_unchecked(i);
             let needed = target - num_i;
-            
+
             // SIMD-friendly sequential access
             for j in (i + 1)..len {
                 if *nums.get_unchecked(j) == needed {
@@ -50,7 +49,7 @@ fn two_sum_n_squared(nums: Vec<i32>, target: i32) -> PyResult<Vec<i32>> {
             }
         }
     }
-    
+
     Ok(Vec::new())
 }
 
@@ -60,20 +59,20 @@ fn two_sum_n(nums: Vec<i32>, target: i32) -> PyResult<Vec<i32>> {
     if len < 2 {
         return Ok(Vec::new());
     }
-    
+
     let mut map = HashMap::with_capacity(len);
-    
+
     // Use iterator for better optimization
     for (i, &num) in nums.iter().enumerate() {
         let complement = target - num;
-        
+
         if let Some(&j) = map.get(&complement) {
             return Ok(vec![j as i32, i as i32]);
         }
-        
+
         map.insert(num, i);
     }
-    
+
     Ok(Vec::new())
 }
 
@@ -83,22 +82,22 @@ fn two_sum_n_hashbrown(nums: Vec<i32>, target: i32) -> PyResult<Vec<i32>> {
     if len < 2 {
         return Ok(Vec::new());
     }
-    
+
     let mut map = HBHashMap::with_capacity(len);
-    
+
     // Avoid bounds checking in release mode
     unsafe {
         for (i, &num) in nums.iter().enumerate() {
             let complement = target - num;
-            
+
             if let Some(&j) = map.get(&complement) {
                 return Ok(vec![j as i32, i as i32]);
             }
-            
+
             map.insert(num, i);
         }
     }
-    
+
     Ok(Vec::new())
 }
 
@@ -109,10 +108,10 @@ fn fibonacci_recursive(n: i32) -> PyResult<i32> {
         match n {
             0 => a,
             1 => b,
-            _ => fib_tail(n - 1, b, a + b)
+            _ => fib_tail(n - 1, b, a + b),
         }
     }
-    
+
     Ok(fib_tail(n, 0, 1))
 }
 
@@ -121,17 +120,17 @@ fn fibonacci_hash(n: i32) -> PyResult<i32> {
     if n <= 1 {
         return Ok(n);
     }
-    
+
     let mut map = HashMap::with_capacity((n + 1) as usize);
     map.insert(0, 0);
     map.insert(1, 1);
-    
+
     // Iterative approach is faster than recursive
     for i in 2..=n {
-        let val = map.get(&(i-1)).unwrap() + map.get(&(i-2)).unwrap();
+        let val = map.get(&(i - 1)).unwrap() + map.get(&(i - 2)).unwrap();
         map.insert(i, val);
     }
-    
+
     Ok(*map.get(&n).unwrap())
 }
 
@@ -140,19 +139,19 @@ fn fibonacci_hashbrown(n: i32) -> PyResult<i32> {
     if n <= 1 {
         return Ok(n);
     }
-    
+
     let mut map = HBHashMap::with_capacity((n + 1) as usize);
     map.insert(0, 0);
     map.insert(1, 1);
-    
+
     // Iterative approach with minimal HashMap lookups
     for i in 2..=n {
         unsafe {
-            let val = map.get(&(i-1)).unwrap_unchecked() + map.get(&(i-2)).unwrap_unchecked();
+            let val = map.get(&(i - 1)).unwrap_unchecked() + map.get(&(i - 2)).unwrap_unchecked();
             map.insert(i, val);
         }
     }
-    
+
     Ok(*map.get(&n).unwrap())
 }
 
